6c41ee8e49f9179615fcb2973acac900c9d39055
Masha Evtihevich <evtihevich1996@gmail.com>
2021-07-11 01:42:50 +0300

    feat: add the calc task

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import AppTap from "./AppTap";
+import AppCalc from "./AppCalc";
 
 ReactDOM.render(
   <React.StrictMode>
     <AppTap />
+    <AppCalc />
   </React.StrictMode>,
   document.getElementById("root")
 );
